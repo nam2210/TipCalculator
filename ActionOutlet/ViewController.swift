c61e52a9f16c9bef184dbfeaa7639016f7bda38c
nampham <nam.phho@gmail.com>
2017-01-29 12:00:14 +0700

    Initial Commit

---
+++ ActionOutlet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ActionOutlet
+//
+//  Created by Nam Pham on 1/29/17.
+//  Copyright © 2017 Nam Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
